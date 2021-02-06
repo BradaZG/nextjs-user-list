6b63b98c1f27a4588b2678145aaa093f1abfb962
Domagoj Marusic <brada@VBTmacbook.local>
2021-02-06 13:31:53 +0100

    First version

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright 2021 BradaZG</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='about ninjas' />
+      </Head>
+      <div>
+        <h1>AboutPage</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non dolore
+          voluptate distinctio. Et exercitationem doloribus ea veritatis est?
+          Itaque eligendi nemo quasi nesciunt incidunt doloribus eaque commodi
+          eum ab? Dicta. Earum dolores obcaecati tempora voluptatibus! Aperiam
+          iusto est odit excepturi quibusdam quo molestias blanditiis? Fugit
+          eos, sequi, expedita veniam, commodi saepe nisi iusto repudiandae a
+          culpa cupiditate laborum mollitia iste?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non dolore
+          voluptate distinctio. Et exercitationem doloribus ea veritatis est?
+          Itaque eligendi nemo quasi nesciunt incidunt doloribus eaque commodi
+          eum ab? Dicta. Earum dolores obcaecati tempora voluptatibus! Aperiam
+          iusto est odit excepturi quibusdam quo molestias blanditiis? Fugit
+          eos, sequi, expedita veniam, commodi saepe nisi iusto repudiandae a
+          culpa cupiditate laborum mollitia iste?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non dolore
+          voluptate distinctio. Et exercitationem doloribus ea veritatis est?
+          Itaque eligendi nemo quasi nesciunt incidunt doloribus eaque commodi
+          eum ab? Dicta. Earum dolores obcaecati tempora voluptatibus! Aperiam
+          iusto est odit excepturi quibusdam quo molestias blanditiis? Fugit
+          eos, sequi, expedita veniam, commodi saepe nisi iusto repudiandae a
+          culpa cupiditate laborum mollitia iste?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
